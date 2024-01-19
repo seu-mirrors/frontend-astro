cd9c526a022b266ec4df36dbbd4741aa7bcabf0f
Mole Shang <135e2@135e2.dev>
2024-01-19 19:12:40 +0800

    fix(vite): exclude src/server

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,27 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath } from "node:url";
+import { resolve } as path from "path";
+
+const ExcludeFiles = ["src/server/main.js"];
+const filesPathToExclude = ExcludeFiles.map((src) => {
+  return fileURLToPath(new URL(src, import.meta.url));
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [],
   build: {
+    manifest: true,
+    rollupOptions: {
+      external: [
+        ...filesPathToExclude
+      ]
+    },
     'assetsDir': '-/assets',
-  }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    }
+  },
 })
